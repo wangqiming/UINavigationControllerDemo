292a2fe109f019d3da34ab456d214d8ed122fde5
wangqiming <wangqiming@wangqimingdeMacBook-Pro.local>
2014-09-04 21:42:54 +0800

    Initial Commit

---
+++ UINavigationControllerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UINavigationControllerDemo
+//
+//  Created by wangqiming on 14-9-4.
+//  Copyright (c) 2014年 zju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
